f3b6da969c1f91859a92929af931b23c7471d822
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-05 13:00:20 +0100

    Update store and delete store test completed

--- store.test.js
+++ store.test.js
@@ -5,6 +5,7 @@ dotenv.config({ path: "./config.env" });
 
 const request = supertest("https://staging.api.customerpay.me");
 
+// POST new Store
 it("test store endpoint(POST)", async (done) => {
   const response = await request
     .post("/store/new")
@@ -26,6 +27,7 @@ it("test store endpoint(POST)", async (done) => {
   done();
 });
 
+// GET store endpoints
 it("test store endpoint(GET)", async (done) => {
   const response = await request
     .get(`/store/`)
@@ -39,28 +41,42 @@ it("test store endpoint(GET)", async (done) => {
   done();
 });
 
-// it("test store endpoint(PUT)", async (done) => {
-//   const response = await request
-//     .put("/store/update/")
-//     .set("Accept", "application/json")
-//     .set("x-access-token", process.env.TOKEN)
-//     .set("param", process.env.STORE_ID)
-//     .send({
-//       store_name: process.env.STORE_NAME,
-//       shop_address: process.env.SHOP_ADDRESS,
-//       tagline: process.env.TAGLINE,
-//       phone_number: process.env.PHONE_NUMBER,
-//       email: process.env.STORE_EMAIL,
-//       country: process.env.COUNTRY,
-//       state: process.env.STATE,
-//     })
+// UPDATE Store
+it("test store endpoint(PUT)", async (done) => {
+  const response = await request
+    .put(`/store/update/${process.env.STORE_ID}`)
+    .set("Accept", "application/json")
+    .set("x-access-token", process.env.TOKEN)
+    .send({
+      store_name: process.env.STORE_NAME,
+      shop_address: process.env.SHOP_ADDRESS,
+      tagline: process.env.TAGLINE,
+      phone_number: process.env.PHONE_NUMBER,
+      email: process.env.STORE_EMAIL,
+      country: process.env.COUNTRY,
+      state: process.env.STATE,
+    });
 
-//   expect(response.body.data.statusCode).toBe(404);
-//   expect(response.body.success).toBe(false);
-//   // expect(response.body.message).toBe("Store added successfully");
-//   done();
-// });
+  expect(response.body.data.statusCode).toBe(201);
+  expect(response.body.success).toBe(true);
+  expect(response.body.message).toBe("Store updated successfully");
+  done();
+});
 
+// DELETE Store
+it("test store endpoint(DELETE)", async (done) => {
+  const response = await request
+    .delete(`/store/delete/${process.env.STORE_DELETE_ID}`)
+    .set("Accept", "application/json")
+    .set("x-access-token", process.env.TOKEN);
+
+  expect(response.body.data.statusCode).toBe(200);
+  expect(response.body.success).toBe(true);
+  expect(response.body.message).toBe("Store deleted successfully");
+  done();
+});
+
+// POST new bank for store
 it("test store/bank endpoint(POST)", async (done) => {
   const response = await request
     .post("/store/bank/")
