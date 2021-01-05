c7d0b191fcda20085c2926da953b403930b898c4
Idris Olubisi <heedris2olubisi@gmail.com>
2021-01-05 11:40:40 +0100

    Retrieve store test completed

--- store.test.js
+++ store.test.js
@@ -26,15 +26,38 @@ it("test store endpoint(POST)", async (done) => {
   done();
 });
 
+it("test store endpoint(GET)", async (done) => {
+  const response = await request
+    .get(`/store/`)
+    .set("Accept", "application/json")
+    .set("x-access-token", process.env.TOKEN)
+    .set("param", process.env.STORE_ID);
 
-// it("test store endpoint(GET)", async (done) => {
+  expect(response.body.data.statusCode).toBe(200);
+  expect(response.body.success).toBe(true);
+  expect(response.body.message).toBe("Here are all your stores");
+  done();
+});
+
+
+// it("test store endpoint(PUT)", async (done) => {
 //   const response = await request
-//     .get(`/store/:store_id=${process.env.STORE_ID}`)
+//     .put("/store/update/")
 //     .set("Accept", "application/json")
-//     .set("x-access-token", process.env.TOKEN);
+//     .set("x-access-token", process.env.TOKEN)
+//     .set("param", process.env.STORE_ID)
+//     .send({
+//       store_name: process.env.STORE_NAME,
+//       shop_address: process.env.SHOP_ADDRESS,
+//       tagline: process.env.TAGLINE,
+//       phone_number: process.env.PHONE_NUMBER,
+//       email: process.env.STORE_EMAIL,
+//       country: process.env.COUNTRY,
+//       state: process.env.STATE,
+//     })
 
-//   expect(response.body.data.statusCode).toBe(200);
-//   expect(response.body.success).toBe(true);
-//   expect(response.body.message).toBe("Here are all your stores");
+//   expect(response.body.data.statusCode).toBe(404);
+//   expect(response.body.success).toBe(false);
+//   // expect(response.body.message).toBe("Store added successfully");
 //   done();
 // });
